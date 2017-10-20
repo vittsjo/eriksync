ca1de68e95eff74cbd9aca051e3cf4991361cbcf
user <user@localhost>
2017-10-20 19:57:27 +0800

    Fix bug in utils::expand_user()

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -7,7 +7,7 @@ pub fn home_dir() -> String {
 pub fn expand_user(path: &std::path::Path) -> std::path::PathBuf {
     let mut path_str = String::from(path.to_str().unwrap_or_default());
 
-    if path_str.chars().nth(0).unwrap() == '~' {
+    if path_str.len() > 0 && path_str.chars().nth(0).unwrap() == '~' {
         path_str = path_str.replacen("~", &home_dir(), 1);
     }
 
